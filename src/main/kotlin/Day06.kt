17529a1bbae5070204aedd52be04a9a871a17ae4
Sagar <sagar.viradiya@bookmyshow.com>
2018-02-01 12:21:48 +0530

    Day 06 part 02 solution

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -6,40 +6,58 @@ import utils.toIntList
  */
 object Day06 {
 
-    fun part1(input: String) : Int {
+    fun part1(input: String) = getRedistributionCount(input)
 
-        val intList = input.splitAtWhiteSpace().toIntList().toMutableList()
+    fun part2(input: String) = getRedistributionCount(input, true)
+
+    private fun getRedistributionCount(input: String, secondCycle: Boolean = false) : Int {
+
+        var intList = input.splitAtWhiteSpace().toIntList()
         val seen = mutableSetOf<List<Int>>()
 
-        var maxIndex: Int
-        var maxValue: Int
         var counter = 0
+        var secondLoop = secondCycle
 
-        while (!seen.contains(intList)) {
+        while (!seen.contains(intList) || secondLoop) {
+
+            if (seen.contains(intList)) {
+                seen.clear()
+                secondLoop = false
+                counter = 0
+            }
 
             seen.add(intList)
 
-            maxValue = intList.max() ?: 0
-            maxIndex = intList.indexOf(maxValue)
-            intList[maxIndex++] = 0
-
-
-            while (maxValue != 0) {
-
-                if (maxIndex == intList.size) {
-                    maxIndex = 0
-                }
-
-                intList[maxIndex++]++
-                maxValue--
-
-            }
+            intList = redistributeBlock(intList, intList.max() ?: 0)
 
             counter++
 
         }
 
         return counter
+
+    }
+
+    private fun redistributeBlock(intList: List<Int>, maxValue: Int) : List<Int> {
+
+        val mutableIntList = intList.toMutableList()
+        var steps = maxValue
+        var maxIndex = intList.indexOf(maxValue)
+
+        mutableIntList[maxIndex++] = 0
+
+        while (steps != 0) {
+
+            if (maxIndex == intList.size) {
+                maxIndex = 0
+            }
+
+            mutableIntList[maxIndex++]++
+            steps--
+
+        }
+
+        return mutableIntList
 
     }
 
--- src/test/kotlin/Day06Test.kt
+++ src/test/kotlin/Day06Test.kt
@@ -13,8 +13,18 @@ class Day06Test {
     }
 
     @Test
-    fun `part 01 actual test`(){
+    fun `part 01 actual test`() {
         Assert.assertEquals(12841, Day06.part1(getInput("day06.txt")))
+    }
+
+    @Test
+    fun `part 02 sample test`() {
+        Assert.assertEquals(4, Day06.part2(getInput("day06sample.txt")))
+    }
+
+    @Test
+    fun `part 02 actual test`() {
+        Assert.assertEquals(8038, Day06.part2(getInput("day06.txt")))
     }
 
 }
