31caf1690aea362d6ee6cbfa9b4c0c8114367efb
Sagar <sagar.viradiya@bookmyshow.com>
2018-07-25 19:23:06 +0530

    Day 16 part 2 solution
     - Part 2 implementation
     - Part 2 test

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -12,7 +12,7 @@ object Day16 {
             danceMoveType = getDanceMoveType(danceMove)
             when(danceMoveType) {
                 is DanceMoveType.Spin -> {
-                    val splitAt = input.length - danceMoveType.spins
+                    val splitAt = programs.length - danceMoveType.spins
                     val firstPart = programs.substring(0 until splitAt)
                     programs.replace(0, splitAt, "")
                     programs.append(firstPart)
@@ -35,6 +35,58 @@ object Day16 {
         return programs.toString()
     }
 
+
+    /**
+     * Old solution using caching to reduce computation.
+     * Not optimise as it is iterating 1 billion times!
+     * */
+    /*fun part2(danceMoves: String, input: String = "abcdefghijklmnop", danceRepeat: Long = 1000000000): String {
+
+        var _input = input
+        val danceMapping = mutableMapOf<String, String>()
+
+        for (i in 0 until danceRepeat) {
+
+            if (danceMapping[_input] == null) {
+                danceMapping[_input] = part1(danceMoves, _input)
+            }
+            _input = danceMapping[_input]!!
+
+        }
+
+        return _input
+
+    }*/
+
+    /**
+     * Original solution was to iterate 1 billion times! and cache the result of dance sequence.
+     *
+     * This solution is optimised as it has much less iterations.
+     * Copied form https://github.com/dlew/aoc-2017/blob/answers/src/main/kotlin/Day16.kt
+     * */
+    fun part2(danceMoves: String, input: String = "abcdefghijklmnop", danceRepeat: Int = 1000000000): String {
+
+        var _input = input
+        for (i in 0 until danceRepeat % findCycleLength(danceMoves, input)) {
+            _input = part1(danceMoves, _input)
+        }
+        return _input
+
+    }
+
+    private fun findCycleLength(danceMoves: String, input: String): Int {
+        var _input = input
+        val seen = mutableSetOf<String>()
+        var length = 0
+        while (_input !in seen) {
+            seen.add(_input)
+            _input = part1(danceMoves, _input)
+            length++
+        }
+
+        return length
+    }
+
     private fun getDanceMoveType(input: String): DanceMoveType {
         return when(input[0]) {
             's' -> DanceMoveType.Spin(input.substring(1).toInt())
@@ -48,7 +100,6 @@ object Day16 {
             else -> throw IllegalArgumentException("Invalid dance move")
         }
     }
-
 
     sealed class DanceMoveType {
         class Spin(val spins: Int): DanceMoveType()
--- src/test/kotlin/Day16Test.kt
+++ src/test/kotlin/Day16Test.kt
@@ -13,4 +13,14 @@ class Day16Test {
     fun `part 1 actual test`() {
         Assert.assertEquals("doeaimlbnpjchfkg", Day16.part1(getInput("day16.txt")))
     }
+
+    @Test
+    fun `part 2 sample test`() {
+        Assert.assertEquals("ceadb", Day16.part2(getInput("day16sample.txt"), "abcde", 2))
+    }
+
+    @Test
+    fun `part 2 actual test`() {
+        Assert.assertEquals("agndefjhibklmocp", Day16.part2(getInput("day16.txt")))
+    }
 }
