7646c7e34246a4650aced08f9c51c1ba3579904d
Sagar Viradiya <sagarviradiya4444@gmail.com>
2018-12-30 18:28:42 +0530

    Day 19 part 2 solution
     - Part 2 implementation
     - Part 2 test

--- src/main/kotlin/Day19.kt
+++ src/main/kotlin/Day19.kt
@@ -1,20 +1,22 @@
 object Day19 {
 
-    fun part1(input: String): String {
+    fun part1(input: String, shouldCountStep: Boolean = false): Any {
         val lines = input.lines()
         val charMatrix = lines.map { it.toCharArray() }
         var direction: Direction = Direction.DOWN
         var i = 0
         var j = lines[0].indexOf('|')
-        var end  = false
+        var end = false
         val path = StringBuffer()
+        var steps = 0
         while (!end) {
-            when(direction) {
+            steps++
+            when (direction) {
                 is Direction.UP -> {
                     i--
                     if (i >= 0 && j < charMatrix[i].size) {
                         if (charMatrix[i][j].toInt() == 32) {
-                          end = true
+                            end = true
                         } else if (charMatrix[i][j] == '+') {
                             direction = when {
                                 j == 0 -> Direction.RIGHT
@@ -22,7 +24,7 @@ object Day19 {
                                 charMatrix[i][j - 1].toInt() != 32 -> Direction.LEFT
                                 else -> Direction.RIGHT
                             }
-                        } else if (charMatrix[i][j] != '-' && charMatrix[i][j] != '|') {
+                        } else if (!shouldCountStep && charMatrix[i][j] != '-' && charMatrix[i][j] != '|') {
                             path.append(charMatrix[i][j])
                         }
                     } else {
@@ -42,7 +44,7 @@ object Day19 {
                                 charMatrix[i][j - 1].toInt() != 32 -> Direction.LEFT
                                 else -> Direction.RIGHT
                             }
-                        } else if (charMatrix[i][j] != '-' && charMatrix[i][j] != '|') {
+                        } else if (!shouldCountStep && charMatrix[i][j] != '-' && charMatrix[i][j] != '|') {
                             path.append(charMatrix[i][j])
                         }
                     } else {
@@ -59,7 +61,7 @@ object Day19 {
                                 j < charMatrix[i + 1].size && charMatrix[i + 1][j].toInt() != 32 -> Direction.DOWN
                                 else -> Direction.UP
                             }
-                        } else if (charMatrix[i][j] != '-' && charMatrix[i][j] != '|') {
+                        } else if (!shouldCountStep && charMatrix[i][j] != '-' && charMatrix[i][j] != '|') {
                             path.append(charMatrix[i][j])
                         }
                     } else {
@@ -68,7 +70,7 @@ object Day19 {
                 }
                 is Direction.RIGHT -> {
                     j++
-                    if(j < charMatrix[i].size && charMatrix[i][j].toInt() != 32) {
+                    if (j < charMatrix[i].size && charMatrix[i][j].toInt() != 32) {
                         if (charMatrix[i][j] == '+') {
                             direction = when {
                                 i == 1 -> Direction.DOWN
@@ -76,7 +78,7 @@ object Day19 {
                                 j < charMatrix[i + 1].size && charMatrix[i + 1][j].toInt() != 32 -> Direction.DOWN
                                 else -> Direction.UP
                             }
-                        } else if (charMatrix[i][j] != '-' && charMatrix[i][j] != '|') {
+                        } else if (!shouldCountStep && charMatrix[i][j] != '-' && charMatrix[i][j] != '|') {
                             path.append(charMatrix[i][j])
                         }
                     } else {
@@ -85,7 +87,11 @@ object Day19 {
                 }
             }
         }
-        return path.toString()
+        return if (shouldCountStep) steps else path.toString()
+    }
+
+    fun part2(input: String): Int {
+        return part1(input, true) as Int
     }
 
     sealed class Direction {
--- src/test/kotlin/Day19Test.kt
+++ src/test/kotlin/Day19Test.kt
@@ -13,4 +13,14 @@ class Day19Test {
     fun `part 1 actual test`() {
         Assert.assertEquals("GINOWKYXH", Day19.part1(getInput("day19.txt")))
     }
+
+    @Test
+    fun `part 2 sample test`() {
+        Assert.assertEquals(46, Day19.part2(getInput("day19sample.txt")))
+    }
+
+    @Test
+    fun `part 2 actual test`() {
+        Assert.assertEquals(16636, Day19.part2(getInput("day19.txt")))
+    }
 }
